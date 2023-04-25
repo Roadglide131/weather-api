166288b0c8cfb69360375ec281368057899b447b
Roadglide131 <darrenj@oldschoolrenovation.com>
2023-04-25 16:35:54 -0500

    display temp and humidity on today's card

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function () {
   var api_key = "68c676a94fee27a6503ddde8539c7971";
   var api_url = "https://api.openweathermap.org/data/2.5/forecast?q=";
+  function kelvin_to_fahrenheit(value) {
+    return (((value - 273.15) * 9) / 5 + 32).toFixed(2);
+  }
   function fetch_city(city) {
     fetch(api_url + city + "&appid=" + api_key)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
+        var today = data.list[0];
+        $("#state_date").text(today.dt_txt + " " + city);
+        $("#temp").text("temp: " + kelvin_to_fahrenheit(today.main.temp));
+        $("#humidity").text("humidity: " + today.main.humidity);
         for (var i = 0; i < data.list.length; i++) {
           var weather_data = data.list[i];
           console.log(weather_data);
-          var kelvin_to_fahrenheit = (
-            ((weather_data.main.temp - 273.15) * 9) / 5 +
-            32
-          ).toFixed(2);
-          console.log(kelvin_to_fahrenheit);
+          var kf = kelvin_to_fahrenheit(weather_data.main.temp);
+          console.log(kf);
         }
       });
   }
